f2e2c6b57012af2cd62e02a85d143fe5a93f02f3
J.C. Hiatt <jchiatt@me.com>
2017-11-03 01:38:40 -0500

    v 0.1.0 deployment - 11/3/17 1:38am

--- build/js/app.js
+++ build/js/app.js
@@ -1,7 +1,38 @@
 $( document ).ready( function() {
+  
+  // Mobile Menu Icon Handler
   $( '.menu-icon' ).click( function(e) {
     e.preventDefault();
     $( this ).toggleClass( 'open' );
     $('.nav-container').toggleClass( 'open' );
   });
+
+  // FAQ Show/Hide
+  $( '.question' ).click( function() {
+    $( this ).parent().find(' .answer ').slideToggle();
+  });  
+
+  // Order Submission
+  $( '#orderForm' ).submit( function(e) {
+    e.preventDefault();
+    $( '.order-form' ).slideToggle();
+    $( '.loader' ).show();
+
+
+    $.ajax({
+      url: 'https://hooks.zapier.com/hooks/catch/1776164/i716nq/',
+      type: 'post',
+      data: $( '#orderForm' ).serialize(),
+      success: function() {
+        $( '.loader' ).hide();
+        $( '.thank-you' ).slideToggle();
+        decrementDiscounts();
+      },
+      error: function() {
+        $( '.loader' ).hide();
+        $( '.error' ).slideToggle();
+      }
+    });
+  });
+
 });
