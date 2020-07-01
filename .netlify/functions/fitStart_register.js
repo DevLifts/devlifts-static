40161bb402b7dc1e36caf695ae0b9ebd7b67eec9
J.C. Hiatt <jc@jchiatt.com>
2020-06-30 22:24:05 -0500

    hook up zapier to netlify functions

--- .netlify/functions/fitStart_register.js
+++ .netlify/functions/fitStart_register.js
@@ -1,8 +1,40 @@
+const fetch = require('node-fetch');
 const queryString = require('query-string');
 
+const ZAPIER_ENDPOINT = "https://hooks.zapier.com/hooks/catch/1776164/o80fkvy";
+
 exports.handler = async (event, context, callback) => {
-  console.log('### BODY', queryString.parse(event.body))
-  callback(null, {
-    statusCode: 200
+  const parsedBody = queryString.parse(event.body);
+  
+  if (!parsedBody) {
+    callback(new Error("Missing required fields."), { statusCode: 400 })
+    return;
+  }
+
+  if (!parsedBody.Email) {
+    callback(new Error("Missing email address."), { statusCode: 400 })
+    return;
+  }
+
+  if ( parsedBody.Product !== "fit.Start(lean)" || parsedBody.Product !== "fit.Start(bodyweight)" || parsedBody.Product !== "fit.Start(strong)") {
+    callback(new Error("Bad bot."), { statusCode: 400 })
+    return;
+  }
+
+  fetch(ZAPIER_ENDPOINT, {
+    method: "POST",
+    body: parsedBody,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(() => {
+    callback(null, {
+      statusCode: 200
+    });
+  }).catch(err => {
+    callback(new Error("Error sending data to Zapier. Please try again."), {
+      statusCode: err.statusCode || 500,
+    });
   });
 }
